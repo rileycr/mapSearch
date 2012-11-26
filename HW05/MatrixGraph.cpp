bf277706d03e1f365b9b100f0d6ad577c84ac0fe
Cooper Riley <rileycr@muohio.edu>
2012-11-26 12:32:01 -0500

    Added method headers to MatrixGraph.cpp file

---
+++ HW05/MatrixGraph.cpp
@@ -0,0 +1,30 @@
+#include "MatrixGraph.h"
+
+
+MatrixGraph::MatrixGraph(unsigned num_nodes){
+	EList* mGraph = new EList[num_nodes];
+}
+
+void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
+	
+}
+
+EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const{
+	
+}
+
+std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
+
+}
+
+unsigned MatrixGraph::degree(NodeID u) const{
+
+}
+
+unsigned MatrixGraph::size() const{
+
+}
+
+unsigned MatrixGraph::numEdges() const{
+
+}
