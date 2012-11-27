d8c03df69ba45ab99c9d38db57c19aa76a7b87bb
Cooper Riley <rileycr@muohio.edu>
2012-11-27 17:36:13 -0500

    Continued work on MatrixGraph.cpp

--- HW05/MatrixGraph.cpp
+++ HW05/MatrixGraph.cpp
@@ -2,19 +2,23 @@
 
 
 MatrixGraph::MatrixGraph(unsigned num_nodes){
-	EList* mGraph = new EList[num_nodes];
+	num_edges = 0;
+	EList mGraph(num_nodes);
 }
 
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
-	
+	M.at(u).at(v) = weight;
+
+	num_edges++;
 }
 
 EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const{
-	
+	return M.at(u).at(v);
 }
 
+//TODO Not sure if this works but it compiles
 std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
-
+	return EList(u);
 }
 
 unsigned MatrixGraph::degree(NodeID u) const{
@@ -26,5 +30,5 @@ unsigned MatrixGraph::size() const{
 }
 
 unsigned MatrixGraph::numEdges() const{
-
+	return num_edges;
 }
