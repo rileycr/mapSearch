795912ec14ba6acda590fbba0a434ad03b30da9c
Cooper Riley <rileycr@muohio.edu>
2012-11-27 19:13:24 -0500

    Added comments to top of MatrixGraph.cpp
    Also continued working on MatrixGraph.cpp

--- HW05/MatrixGraph.cpp
+++ HW05/MatrixGraph.cpp
@@ -1,9 +1,19 @@
+/**
+* @file MatrixGraph.cpp
+* CSE 274 - Fall 2012
+*
+* @author Cooper Riley
+*
+* @note This is the implimentation of the MatrixGraph.h provided for 
+* this assignment.
+*/
+
 #include "MatrixGraph.h"
 
 
 MatrixGraph::MatrixGraph(unsigned num_nodes){
 	num_edges = 0;
-	EList mGraph(num_nodes);
+	M.resize(num_nodes);
 }
 
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
@@ -22,11 +32,20 @@ std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
 }
 
 unsigned MatrixGraph::degree(NodeID u) const{
-
+	int degree = 0;
+	if(0 <= u && u < M.size()){
+		for(int i = 0; i < M.at(u).size() ; i++){
+			if(M.at(u).at(i) != 0)
+				degree++;
+		}
+	} else {
+		return;
+	}
+	return degree;
 }
 
 unsigned MatrixGraph::size() const{
-
+	return M.size();
 }
 
 unsigned MatrixGraph::numEdges() const{
