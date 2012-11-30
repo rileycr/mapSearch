75c03eba80c7092101d111baf7f09168385f82b1
Cooper Riley <rileycr@muohio.edu>
2012-11-30 00:50:28 -0500

    starting on TSP implimentation

--- HW05/GraphAlgs.cpp
+++ HW05/GraphAlgs.cpp
@@ -9,7 +9,22 @@
 */
 
 #include "GraphAlgs.h"
+int* bestTour_;
+float bestTourLength_;
 
 std::pair<std::vector<NodeID>, EdgeWeight> TSP(Graph* G){
 
+
+}
+//Pseudo-code foundation from lecture 11/20
+void tour(int* arr, int size, int startingPoint){
+	if(size - startingPoint == 1){
+		//check if arr is better than bestTour
+	} else {
+		for(int i = startingPoint; i < size; i ++){
+			//swap(arr(startingPoint), arr(i));
+			tour(arr, size, startingPoint++);
+			//swap(arr(startingPoint), arr(i));
+		}
+	}
 }
